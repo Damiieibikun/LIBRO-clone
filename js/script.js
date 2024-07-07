2486195c5b5e7d06b830e2041cf9e902a4549bf0
Damilola Ibikunle <damiieibikun@gmail.com>
2024-07-07 15:46:14 +0100

    added finishing touches to website

--- js/script.js
+++ js/script.js
@@ -1,48 +1,49 @@
-$(document).ready(function() {
-    $('#close').click(function() {
-        $('#side-nav').animate({ 'width': '0%', 'height': '0%' })
-    })
+$(document).ready(function () {
+  $("#close").click(function () {
+    $("#side-nav").animate({ width: "0%", height: "0%" });
+  });
 
-    $('#menu').click(function() {
-        $('#side-nav').animate({ 'width': '100%', 'height': '100%' })
-    })
+  $("#menu").click(function () {
+    $("#side-nav").animate({ width: "100%", height: "100%" });
+  });
 
-    $('#menu').hover(function() {
-        $('hr').animate({ 'margin': '7px' }, 200)
-    }, function() {
-        $('hr').animate({ 'margin': '5px' }, 200)
-    })
-
-
-    function fadeImages() {
-        $('#banner1').fadeOut(5000, function() {
-            $('#banner2').fadeIn(3000, function() {
-                $('#banner1').fadeIn(3000, fadeImages);
-
-            })
-        })
-
-    }
-    // fadeImages()
-
-})
-
-$('.div-card').hover(function() {
-        $(this).find('img').css('transform', 'scale(1.1)')
+  $("#menu").hover(
+    function () {
+      $("hr").animate({ margin: "7px" }, 200);
     },
-    function() {
-        $(this).find('img').css('transform', 'scale(1)')
-    })
+    function () {
+      $("hr").animate({ margin: "5px" }, 200);
+    }
+  );
 
-$('.div-card').click(function() {
-    $('#descriptions').hide()
-    $('#descriptions2').show()
-})
+  function fadeImages() {
+    $("#banner1").fadeOut(5000, function () {
+      $("#banner2").fadeIn(3000, function () {
+        $("#banner1").fadeIn(3000, fadeImages);
+      });
+    });
+  }
+  fadeImages();
+});
 
-$('#btn').click(function() {
-    $('#btn').css({
-        'background-color': '#4bea9b',
-        'color': 'black',
-        'border-color': '#4bea9b'
-    })
-})
+$(".div-card").hover(
+  function () {
+    $(this).find("img").css("transform", "scale(1.1)");
+  },
+  function () {
+    $(this).find("img").css("transform", "scale(1)");
+  }
+);
+
+$(".div-card").click(function () {
+  $("#descriptions").hide();
+  $("#descriptions2").show();
+});
+
+$("#btn").click(function () {
+  $("#btn").css({
+    "background-color": "#4bea9b",
+    color: "black",
+    "border-color": "#4bea9b",
+  });
+});
